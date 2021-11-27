a59d15500b412094ac1c14dadfb165f58d3f3fc8
Fionn McGreal <84995027+Fionn-McGreal@users.noreply.github.com>
2021-11-26 17:10:00 -0700

    Add files via upload

---
+++ Plotly-Challenge/Plotly-Challenge/static/js/app.js
@@ -0,0 +1,74 @@
+function loadCharts(id){
+// # Read in json
+    d3.json("samples.json").then((data)=>{
+
+        console.log(data)
+        
+        let filterData = data.samples.filter(obj => obj.id == id);
+        //console.log(filterData)
+        let otu_ids = filterData[0].otu_ids;
+        let otu_labels = filterData[0].otu_labels;
+        let sample_values = filterData[0].sample_values;
+
+//  # Assemble Bar Chart
+        let bar = [{
+            type: 'bar',
+            x: sample_values.slice(0,20).reverse(),
+            y: otu_ids.map(otu => "OTU " + otu).slice(0,20).reverse(),
+            text: otu_labels.slice(0,20).reverse(),
+            orientation: 'h',
+            order: 'ascending'
+          }];
+          
+          Plotly.newPlot('bar', bar);
+
+
+          let trace1 = {
+            x: otu_ids,
+            y: sample_values,
+            text:otu_labels,
+            mode: 'markers',
+            marker: {
+              color: otu_ids,
+              size: sample_values
+            } };
+          
+          let bubble = [trace1];
+          
+          let layout = {
+            title: 'Belly Button Biodiversity',
+            showlegend: false,
+            height: 650,
+            width: 1200
+          };
+          // # Assemble Bubble Chart
+          Plotly.newPlot('bubble', bubble, layout);
+
+
+          
+      let metaData = data.metadata.filter(obj => obj.id == id);
+      console.log(metaData) 
+
+      let panel = d3.select("#sample-metadata")
+      for (const [key, value] of Object.entries(metaData[0])) {
+        panel.append("h5").text(key + ": " + value)
+      }
+    })
+
+
+}
+
+
+d3.json("samples.json").then((data) => {
+
+    let dropdown = d3.select("#selDataset")
+
+    data.names.forEach((id) =>{
+        dropdown.append("option").text(id).property("value", id)
+    })
+
+    loadCharts(data.names[0])});
+
+function optionChanged(selectedID){
+    console.log(selectedID)
+    loadCharts(selectedID)};
